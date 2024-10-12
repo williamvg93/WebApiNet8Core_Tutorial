784d2697e33c4067cc82a6e417132f6d1b623bc0
williamvg93 <sonico_5071@hotmail.com>
2024-10-11 19:53:50 -0500

    feat: :sparkles: The classes for the Employee and Rol entites was created

---
+++ WebApi/Entities/Employee.cs
@@ -0,0 +1,12 @@
+﻿
+namespace WebApi.Entities
+{
+    public class Employee
+    {
+        public int IdEmployee { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public int FkIdRol {  get; set; }
+        public virtual Rol Rol { get; set; }
+    }
+}
---
+++ WebApi/Entities/Rol.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace WebApi.Entities
+{
+    public class Rol
+    {
+        public int IdRol { get; set; }
+        public string Name { get; set; }
+        public ICollection<Employee> Employees { get; set; }
+    }
+}
