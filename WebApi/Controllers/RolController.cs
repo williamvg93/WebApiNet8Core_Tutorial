5458e05b063bd5d0751a58f022e3c629c36d61cb
williamvg93 <sonico_5071@hotmail.com>
2024-10-17 16:13:12 -0500

    The Rol Entity Controller was created and GetRolesList EndPoint was created

---
+++ WebApi/Controllers/RolController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApi.Context;
+using WebApi.Entities;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RolController : ControllerBase
+    {
+        private readonly WebApiDbContext _context;
+
+        // Inyección de dependencias
+        public RolController(WebApiDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("GetAllRoles")]
+        public async Task<ActionResult<List<Rol>>> Get()
+        {
+            var rolesList = await _context.Roles.ToListAsync();
+            return Ok(rolesList);
+        }
+
+
+    }
+}
